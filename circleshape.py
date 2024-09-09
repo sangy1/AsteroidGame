f8c6af96d5dd60e20f27fac493f7d7a22da61e2b
SangWon <sangwoncode123@gmail.com>
2024-09-08 21:12:00 -0400

    feat: add player collision with asteroid

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def checkCollision(self, circleshape):
+        return self.position.distance_to(circleshape.position) <= (self.radius + circleshape.radius)
+        
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
     dt = 0
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -21,6 +32,11 @@ def main():
         for obj in updatable:
             obj.update(dt)
         
+        for asteroid in asteroids:
+            if asteroid.checkCollision(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
 
         for obj in drawable:
@@ -31,5 +47,7 @@ def main():
         # Limit framerate to 60 fps
         dt = clock.tick(60) / 1000
     
+    
+
 if __name__ == "__main__":
     main()
